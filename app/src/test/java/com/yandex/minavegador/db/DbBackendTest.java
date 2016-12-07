4cb0e8d98569d6336df46c0283b211dd1aa09fce
Dmitriy V. Popov <busylee@yandex-team.ru>
2016-12-07 23:50:36 +0300

    added test for DbBackeng suggestion

--- app/src/test/java/com/yandex/minavegador/db/DbBackendTest.java
+++ app/src/test/java/com/yandex/minavegador/db/DbBackendTest.java
@@ -5,11 +5,16 @@ import android.database.sqlite.SQLiteDatabase;
 import com.yandex.minavegador.BuildConfig;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by user on 31/07/2016.
@@ -18,13 +23,20 @@ import org.robolectric.annotation.Config;
 @Config(constants = BuildConfig.class)
 public class DbBackendTest implements DbContract {
 
+    DbOpenHelper helper;
+    SQLiteDatabase db;
+    DbBackend dbBackend;
+
+    @Before
+    public void setUp() {
+        helper = new DbOpenHelper(RuntimeEnvironment.application);
+        db = helper.getWritableDatabase();
+
+        dbBackend = new DbBackend(helper);
+    }
+
     @Test
     public void testInsertHistoryItem() {
-        DbOpenHelper helper = new DbOpenHelper(RuntimeEnvironment.application);
-        SQLiteDatabase db = helper.getWritableDatabase();
-
-        DbBackend dbBackend = new DbBackend(helper);
-
         // No page, no history item.
         // Both rows must be inserted.
         dbBackend.insertHistoryItem("yandex.ru");
@@ -40,11 +52,11 @@ public class DbBackendTest implements DbContract {
 
         // Has page, has history item.
         // LastAccess must be updated (1 second precision).
-        long oldLastAccess = getOneField(db, HISTORY, History.LAST_VISITED);
+        long oldLastAccess = getOneFieldLong(db, HISTORY, History.LAST_VISITED);
         dbBackend.insertHistoryItem("yandex.ru");
         Assert.assertEquals(1, getCount(db, HISTORY));
         Assert.assertEquals(1, getCount(db, PAGES));
-        long newLastAccess = getOneField(db, HISTORY, History.LAST_VISITED);
+        long newLastAccess = getOneFieldLong(db, HISTORY, History.LAST_VISITED);
         Assert.assertTrue(Math.abs(newLastAccess - oldLastAccess) < 1000);
 
         // Check the second history item and the value of last access.
@@ -52,10 +64,46 @@ public class DbBackendTest implements DbContract {
         dbBackend.insertHistoryItem("yandex.com");
         Assert.assertEquals(2, getCount(db, HISTORY));
         Assert.assertEquals(2, getCount(db, PAGES));
-        final long lastAccess = getOneField(db, HISTORY, History.LAST_VISITED);
+        final long lastAccess = getOneFieldLong(db, HISTORY, History.LAST_VISITED);
         Assert.assertTrue(Math.abs(was - lastAccess) < 1000);
         final long now = System.currentTimeMillis();
         Assert.assertTrue(Math.abs(now - lastAccess) < 1000);
+    }
+
+    @Test
+    public void testHistorySuggestions() {
+        final String yandexItem = "yandex.com";
+        final String yahooItem = "yahoo.com";
+
+        //should find yandex item
+        dbBackend.insertHistoryItem(yandexItem);
+        Assert.assertEquals(Collections.singletonList(yandexItem),
+                DbUtils.getResultStringListAndClose(
+                        dbBackend.getHistorySuggestions("ya"), Pages.PAGE_URL
+                )
+        );
+
+        //should find both yahoo and yandex
+        dbBackend.insertHistoryItem(yahooItem);
+        Assert.assertEquals(Arrays.asList(yahooItem, yandexItem),
+                DbUtils.getResultStringListAndClose(
+                        dbBackend.getHistorySuggestions("ya"), Pages.PAGE_URL
+                )
+        );
+
+        //should find only yandex
+        Assert.assertEquals(Collections.singletonList(yandexItem),
+                DbUtils.getResultStringListAndClose(
+                        dbBackend.getHistorySuggestions("yan"), Pages.PAGE_URL
+                )
+        );
+
+        //should not find any
+        Assert.assertEquals(Collections.EMPTY_LIST,
+                DbUtils.getResultStringListAndClose(
+                        dbBackend.getHistorySuggestions("go"), Pages.PAGE_URL
+                )
+        );
     }
 
     private int getCount(SQLiteDatabase db, String table) {
@@ -63,8 +111,10 @@ public class DbBackendTest implements DbContract {
                 db.rawQuery("select count(*) from " + table, null)).intValue();
     }
 
-    private long getOneField(SQLiteDatabase db, String table, String field) {
+    private long getOneFieldLong(SQLiteDatabase db, String table, String field) {
         return DbUtils.getResultLongAndClose(
                 db.rawQuery("select " + field + " from " + table, null));
     }
+
+
 }
